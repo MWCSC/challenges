8d46399cb9b1f8db656028055e7bb9b329e29023
Anthony Toyco <amieltoyco@gmail.com>
2022-12-06 11:35:18 -0500

    fixed day 2

--- advanced/days/2/solutions/anthonytoyco/anthonytoyco.py
+++ advanced/days/2/solutions/anthonytoyco/anthonytoyco.py
@@ -1,20 +1,25 @@
-file = open(
-    "input.txt", "r"
-).readlines()
+FILE = open("input.txt", "r").readlines()
 
-h_positions = []
-for line in file:
-    h_positions.append(line.find("h"))
+# find and add the position of h on each line to list
+hpos = []
+for line in FILE:
+    hpos.append(line.find("h"))
 
+# sum of all the h positions
+sum_hpos = 0
+for i in hpos:
+    sum_hpos += i
+
+# find and store the value after each letter h for each line
 values = []
-for line, i in enumerate(file):
-    values.append(file[line][h_positions[line] + 1])
+for line in range(len(FILE)):
+    values.append(FILE[line][hpos[line] + 1])
+# convert the value list into one string
 int_values = "".join(values)
 
-sum_values = 0
+# find the product of all the values
 product_values = 1
 for i in int_values:
-    sum_values += int(i)
     product_values *= int(i)
 
-print(sum_values * product_values)
+print(sum_hpos * product_values)
