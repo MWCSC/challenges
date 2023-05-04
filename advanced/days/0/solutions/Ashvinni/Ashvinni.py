4916662f161becf0fcae31e855f576645d982c36
Ashh0405 <115811013+Ashh0405@users.noreply.github.com>
2023-05-04 17:51:38 -0400

    Update Ashvinni.py

--- advanced/days/0/solutions/Ashvinni/Ashvinni.py
+++ advanced/days/0/solutions/Ashvinni/Ashvinni.py
@@ -1,9 +1,11 @@
- b = 2546375247
+import math
+
+b = 2546375247
 h = 45987482082
-area = (b*h)/2
-c=b**2+h**2
-math.sqrt(c)
-perimeter = h + b + c
-print(area)
-print(perimeter)
-      
+
+area = (b * h) / 2
+c = math.sqrt((b * b) + (h * h))
+perimeter = b + h + c
+
+print(int(area))
+print(int(perimeter))
